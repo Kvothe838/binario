a12f53280b79cba2eaab2769ca03488326b7e552
kvothe838 <facundo@omniasalud.com>
2022-01-20 14:43:31 -0300

    Armado de Casilla y printeo de tablero real

--- src/binario/main.go
+++ src/binario/main.go
@@ -5,21 +5,23 @@ import (
 )
 
 type Valor uint8
-type Tablero [][]Valor
+type Casilla struct {
+	valor       Valor
+	descubierta bool
+}
+type Tablero [][]Casilla
 
 func main() {
-	tablero := make(Tablero, 0)
-	cantidadColumnas := 10
-	cantidadFilas := 10
-
-	for i := 0; i < cantidadColumnas; i++ {
-		columna := make([]Valor, 0)
-		for j := 0; j < cantidadFilas; j++ {
-			columna = append(columna, 1)
-		}
-
-		tablero = append(tablero, columna)
-
+	vacio := Casilla{2, false}
+	cero := Casilla{0, true}
+	uno := Casilla{1, true}
+	var tablero Tablero = [][]Casilla{
+		{uno, vacio, vacio, vacio, vacio, vacio},
+		{uno, uno, vacio, vacio, uno, vacio},
+		{cero, uno, uno, vacio, cero, uno},
+		{vacio, vacio, vacio, vacio, vacio, uno},
+		{cero, vacio, uno, vacio, uno, vacio},
+		{cero, cero, vacio, vacio, cero, uno},
 	}
 
 	tablero.Imprimir()
@@ -28,7 +30,18 @@ func main() {
 func (tablero Tablero) Imprimir() {
 	for i := 0; i < len(tablero); i++ {
 		for j := 0; j < len(tablero[i]); j++ {
-			fmt.Printf("|%v|", tablero[i][j])
+			fmt.Print("|")
+
+			casilla := tablero[i][j]
+			if casilla.descubierta {
+				fmt.Print(casilla.valor)
+			} else {
+				fmt.Print(" ")
+			}
+
+			if j == len(tablero[i])-1 {
+				fmt.Print("|")
+			}
 		}
 		fmt.Println("")
 	}
