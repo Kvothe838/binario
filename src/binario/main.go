211bd4eccc3c7cf5830f1b6ba8fb5644853d3587
kvothe838 <facundo@omniasalud.com>
2022-01-20 16:48:14 -0300

    Añadido ejemplo de tablero más complejo y resuelto por dos seguidos horizontal

--- src/binario/main.go
+++ src/binario/main.go
@@ -6,25 +6,48 @@ import (
 
 type Valor uint8
 type Casilla struct {
-	valor       Valor
-	descubierta bool
+	valor   Valor
+	visible bool
 }
 type Tablero [][]Casilla
 
 func main() {
+	tablero := ArmarTablero()
+	fmt.Println("Tablero inicial")
+	fmt.Println()
+	tablero.Imprimir()
+
+	fmt.Println()
+	fmt.Println()
+
+	tableroArmado := tablero.Resolver()
+	fmt.Println("Tablero resuelto")
+	fmt.Println()
+	tableroArmado.Imprimir()
+}
+
+func ArmarTablero() Tablero {
 	vacio := Casilla{2, false}
 	cero := Casilla{0, true}
 	uno := Casilla{1, true}
 	var tablero Tablero = [][]Casilla{
-		{uno, vacio, vacio, vacio, vacio, vacio},
-		{uno, uno, vacio, vacio, uno, vacio},
-		{cero, uno, uno, vacio, cero, uno},
-		{vacio, vacio, vacio, vacio, vacio, uno},
-		{cero, vacio, uno, vacio, uno, vacio},
-		{cero, cero, vacio, vacio, cero, uno},
+		{vacio, uno, uno, vacio, uno, vacio, vacio, vacio, vacio, vacio, vacio, vacio, uno, vacio},
+		{vacio, vacio, vacio, vacio, vacio, vacio, uno, vacio, vacio, vacio, vacio, cero, vacio, vacio},
+		{uno, vacio, vacio, vacio, cero, cero, vacio, cero, cero, vacio, uno, vacio, vacio, vacio},
+		{vacio, cero, cero, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, uno},
+		{vacio, cero, vacio, vacio, vacio, cero, vacio, vacio, cero, vacio, vacio, vacio, vacio, vacio},
+		{vacio, vacio, vacio, vacio, vacio, cero, vacio, vacio, vacio, vacio, uno, uno, vacio, vacio},
+		{cero, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, uno, vacio, vacio, vacio},
+		{vacio, cero, vacio, vacio, uno, vacio, cero, vacio, cero, vacio, vacio, cero, vacio, vacio},
+		{uno, vacio, vacio, vacio, vacio, vacio, vacio, vacio, cero, vacio, vacio, vacio, uno, vacio},
+		{vacio, vacio, uno, uno, vacio, vacio, vacio, vacio, vacio, uno, vacio, vacio, vacio, vacio},
+		{vacio, cero, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, vacio, uno},
+		{uno, vacio, vacio, cero, vacio, uno, vacio, vacio, cero, vacio, vacio, vacio, vacio, uno},
+		{vacio, vacio, vacio, vacio, vacio, vacio, cero, vacio, cero, cero, vacio, vacio, vacio, vacio},
+		{vacio, vacio, vacio, vacio, vacio, uno, vacio, vacio, vacio, vacio, vacio, uno, vacio, vacio},
 	}
 
-	tablero.Imprimir()
+	return tablero
 }
 
 func (tablero Tablero) Imprimir() {
@@ -33,7 +56,7 @@ func (tablero Tablero) Imprimir() {
 			fmt.Print("|")
 
 			casilla := tablero[i][j]
-			if casilla.descubierta {
+			if casilla.visible {
 				fmt.Print(casilla.valor)
 			} else {
 				fmt.Print(" ")
@@ -45,4 +68,77 @@ func (tablero Tablero) Imprimir() {
 		}
 		fmt.Println("")
 	}
+}
+
+func (tablero Tablero) Resolver() Tablero {
+	tablero = tablero.ResolverDoblesSeguidos()
+	return tablero
+}
+
+func (tablero Tablero) ResolverDoblesSeguidos() Tablero {
+	seguirBarriendo := false
+
+	for !seguirBarriendo {
+		seguirBarriendo = false
+
+		for indiceFila := 0; indiceFila < len(tablero); indiceFila++ {
+			fila := tablero[indiceFila]
+
+			for indiceColumna := 0; indiceColumna < len(fila); indiceColumna++ {
+				casilla := fila[indiceColumna]
+				primerColumna := 0
+				ultimaColumna := len(fila) - 1
+				siguienteColumna := indiceColumna + 1
+				anteriorColumna := indiceColumna - 1
+				opuesto := casilla.valor.ObtenerOpuesto()
+
+				if !casilla.visible || indiceColumna == ultimaColumna {
+					continue
+				}
+
+				siguienteCasilla := fila[siguienteColumna]
+
+				if !siguienteCasilla.visible {
+					continue
+				}
+
+				if casilla.valor != siguienteCasilla.valor {
+					continue
+				}
+
+				if indiceColumna != primerColumna {
+					anteriorCasilla := &fila[anteriorColumna]
+
+					if !anteriorCasilla.visible {
+						anteriorCasilla.valor = opuesto
+						anteriorCasilla.visible = true
+						seguirBarriendo = true
+					}
+
+				}
+
+				if siguienteColumna != ultimaColumna {
+					siguienteSiguienteCasilla := &fila[siguienteColumna+1]
+
+					if !siguienteCasilla.visible {
+						siguienteSiguienteCasilla.valor = opuesto
+						siguienteSiguienteCasilla.visible = true
+						seguirBarriendo = true
+					}
+				}
+			}
+		}
+	}
+
+	return tablero
+}
+
+func (valor Valor) ObtenerOpuesto() Valor {
+	opuestos := map[Valor]Valor{
+		0: 1,
+		1: 0,
+		2: 2,
+	}
+
+	return opuestos[valor]
 }
