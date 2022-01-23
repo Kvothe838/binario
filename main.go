77788b1e5b4cd892d1039389d67bae2b990c1b65
kvothe838 <facundo@omniasalud.com>
2022-01-23 12:03:39 -0300

    Resolver falta uno de un valor horizontal

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ type Casilla struct {
 	visible bool
 }
 type Tablero [][]Casilla
+type Casillas []Casilla
 type Girable func(*bool) Tablero
 
 func main() {
@@ -58,21 +59,26 @@ func ArmarTablero() Tablero {
 
 func (tablero Tablero) Imprimir() {
 	for i := 0; i < len(tablero); i++ {
-		for j := 0; j < len(tablero[i]); j++ {
-			fmt.Print("|")
-
-			casilla := tablero[i][j]
-			if casilla.visible {
-				fmt.Print(casilla.valor)
-			} else {
-				fmt.Print(" ")
-			}
-
-			if j == len(tablero[i])-1 {
-				fmt.Print("|")
-			}
-		}
+		fila := Casillas(tablero[i])
+		fila.Imprimir()
 		fmt.Println("")
+	}
+}
+
+func (casillas Casillas) Imprimir() {
+	for indiceCasilla := 0; indiceCasilla < len(casillas); indiceCasilla++ {
+		fmt.Print("|")
+
+		casilla := casillas[indiceCasilla]
+		if casilla.visible {
+			fmt.Print(casilla.valor)
+		} else {
+			fmt.Print(" ")
+		}
+
+		if indiceCasilla == len(casillas)-1 {
+			fmt.Print("|")
+		}
 	}
 }
 
@@ -100,6 +106,14 @@ func (tablero Tablero) Resolver() Tablero {
 
 		nuevoTablero.ResolverFaltaUnNumero(&volverABarrer)
 		fmt.Println("Falta un numero")
+		fmt.Println()
+		nuevoTablero.Imprimir()
+
+		fmt.Println()
+		fmt.Println()
+
+		nuevoTablero.ResolverFaltaUnoDeUnValor(&volverABarrer)
+		fmt.Println("Falta uno de un valor")
 		fmt.Println()
 		nuevoTablero.Imprimir()
 	}
@@ -184,6 +198,37 @@ func (tablero *Tablero) ResolverFaltaUnNumero(volverABarrerExterno *bool) {
 		seguirBarriendo = true
 		for seguirBarriendo {
 			tablero.ResolverFaltaUnNumeroHorizontal(&seguirBarriendo)
+
+			if seguirBarriendo {
+				volverABarrer = true
+				*volverABarrerExterno = true
+			}
+		}
+
+		tablero.DarVuelta()
+	}
+}
+
+func (tablero *Tablero) ResolverFaltaUnoDeUnValor(volverABarrerExterno *bool) {
+	volverABarrer := true
+
+	for volverABarrer {
+		volverABarrer = false
+		seguirBarriendo := true
+
+		for seguirBarriendo {
+			tablero.ResolverFaltaUnoDeUnValorHorizontal(&seguirBarriendo)
+
+			if seguirBarriendo {
+				*volverABarrerExterno = true
+			}
+		}
+
+		tablero.DarVuelta()
+
+		seguirBarriendo = true
+		for seguirBarriendo {
+			tablero.ResolverFaltaUnoDeUnValorHorizontal(&seguirBarriendo)
 
 			if seguirBarriendo {
 				volverABarrer = true
@@ -293,22 +338,7 @@ func (tablero Tablero) ResolverFaltaUnNumeroHorizontal(seguirBarriendo *bool) Ta
 
 	for indiceFila := 0; indiceFila < lado; indiceFila++ {
 		fila := tablero[indiceFila]
-		vecesEnFilaPorValor := map[Valor]int{
-			0: 0,
-			1: 0,
-			2: 0,
-		}
-
-		hayAlMenosUnoNoVisible := false
-
-		for indiceColumna := 0; indiceColumna < len(fila); indiceColumna++ {
-			casilla := fila[indiceColumna]
-			vecesEnFilaPorValor[casilla.valor]++
-
-			if !casilla.visible {
-				hayAlMenosUnoNoVisible = true
-			}
-		}
+		vecesEnFilaPorValor, hayAlMenosUnoNoVisible := tablero.ObtenerVecesEnFilaPorValor(fila)
 
 		if hayAlMenosUnoNoVisible {
 			var valorACompletar *Valor = nil
@@ -323,18 +353,121 @@ func (tablero Tablero) ResolverFaltaUnNumeroHorizontal(seguirBarriendo *bool) Ta
 				valorACompletar = &cero
 			}
 
-			if valorACompletar != nil {
-				for indiceColumna := 0; indiceColumna < len(fila); indiceColumna++ {
-					casilla := &fila[indiceColumna]
+			if valorACompletar == nil {
+				continue
+			}
 
-					if !casilla.visible {
-						casilla.valor = *valorACompletar
-						casilla.visible = true
-					}
+			for indiceColumna := 0; indiceColumna < len(fila); indiceColumna++ {
+				casilla := &fila[indiceColumna]
+
+				if casilla.visible {
+					continue
 				}
 
-				*seguirBarriendo = true
+				casilla.valor = *valorACompletar
+				casilla.visible = true
 			}
+
+			*seguirBarriendo = true
+		}
+	}
+
+	return tablero
+}
+
+func (tablero Tablero) ObtenerVecesEnFilaPorValor(fila []Casilla) (map[Valor]int, bool) {
+	vecesEnFilaPorValor := map[Valor]int{
+		0: 0,
+		1: 0,
+		2: 0,
+	}
+
+	hayAlMenosUnoNoVisible := false
+
+	for indiceColumna := 0; indiceColumna < len(fila); indiceColumna++ {
+		casilla := fila[indiceColumna]
+		vecesEnFilaPorValor[casilla.valor]++
+
+		if !casilla.visible {
+			hayAlMenosUnoNoVisible = true
+		}
+	}
+
+	return vecesEnFilaPorValor, hayAlMenosUnoNoVisible
+}
+
+func (tablero Tablero) ResolverFaltaUnoDeUnValorHorizontal(seguirBarriendo *bool) Tablero {
+	*seguirBarriendo = false
+	lado := len(tablero)
+	vecesMaximoEnFila := lado / 2
+
+	for indiceFila := 0; indiceFila < lado; indiceFila++ {
+		fila := tablero[indiceFila]
+		vecesEnFilaPorValor, hayAlMenosUnoNoVisible := tablero.ObtenerVecesEnFilaPorValor(fila)
+
+		if !hayAlMenosUnoNoVisible {
+			continue
+		}
+
+		var valorACompletar *Valor = nil
+		cantidadCeros := vecesEnFilaPorValor[0]
+		cantidadUnos := vecesEnFilaPorValor[1]
+
+		if cantidadCeros == vecesMaximoEnFila-1 {
+			uno := Valor(1)
+			valorACompletar = &uno
+		} else if cantidadUnos == vecesMaximoEnFila-1 {
+			cero := Valor(0)
+			valorACompletar = &cero
+		}
+
+		if valorACompletar == nil {
+			continue
+		}
+
+		opuesto := (*valorACompletar).ObtenerOpuesto()
+		filaAux := fila
+		indicesAACtualizar := make([]int, 0)
+
+		for indiceColumna := 0; indiceColumna < lado; indiceColumna++ {
+			casilla := filaAux[indiceColumna]
+
+			if casilla.visible {
+				continue
+			}
+
+			casilla.valor = *valorACompletar
+
+			for indiceColumnaAux := 0; indiceColumnaAux < lado; indiceColumnaAux++ {
+				casillaAux := filaAux[indiceColumnaAux]
+
+				if indiceColumnaAux == indiceColumna || casillaAux.visible {
+					continue
+				}
+
+				casillaAux.valor = opuesto
+			}
+
+			cantidadSeguidos := 0
+
+			for indiceColumnaAux := 0; indiceColumnaAux < lado; indiceColumnaAux++ {
+				casillaAux := filaAux[indiceColumnaAux]
+
+				if casillaAux.valor == *valorACompletar {
+					cantidadSeguidos++
+				}
+			}
+
+			if cantidadSeguidos > 2 {
+				indicesAACtualizar = append(indicesAACtualizar, indiceColumna)
+			}
+
+			filaAux = fila
+		}
+
+		for _, indiceAACtualizar := range indicesAACtualizar {
+			fila[indiceAACtualizar].valor = *valorACompletar
+			fila[indiceAACtualizar].visible = true
 		}
 	}
 
